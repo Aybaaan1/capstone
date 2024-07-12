5d7f72689f1a0d8498f2e3ed045e2b6c73e48561
felamin2023 <kkangpae59@gmail.com>
2024-07-13 02:57:11 +0800

    lost and found page: done

--- app/lostandfound/page.jsx
+++ app/lostandfound/page.jsx
@@ -1,12 +1,78 @@
+"use client";
 import Image from "next/image";
 
 export default function page(){
+
+    const items = [
+        {path: "/imgs/tshirt.png"},
+        {path: "/imgs/lanyards.png"},
+        {path: "/imgs/totebag.png"},
+        {path: "/imgs/flask.png"},
+        {path: "/imgs/tshirt.png"},
+        {path: "/imgs/lanyards.png"},
+        {path: "/imgs/totebag.png"},
+        {path: "/imgs/flask.png"},
+        {path: "/imgs/tshirt.png"},
+        {path: "/imgs/lanyards.png"},
+        {path: "/imgs/totebag.png"},
+        {path: "/imgs/flask.png"}
+    ]
+
+    const sociallinks = [
+        {image: "/imgs/fb-icon.png", href: "/"},
+        {image: "/imgs/twitter-icon.png", href: "/"},
+        {image: "/imgs/instagram-icon.png", href: "/"},
+        {image: "/imgs/youtube-icon.png", href: "/"}
+    ]
+
     return (
-        <div>
+        <div className="mt-10">
             <section
+            // , backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center center"
         style={{ backgroundImage: "url('/imgs/ctulogo.png')" }}
-        className="w-full h-56 mt-10"
+        className="w-full h-56 bg-cover bg-no-repeat bg-center "
       ></section>
+      <section className="mt-10 bg-slate-50 flex flex-col items-center justify-center py-10 gap-10">
+        <div className="flex items-center justify-end gap-32 w-4/5 px-6">
+            <h1 className="font-bold text-5xl">Found Items</h1>
+            <button className="bg-primary text-white px-6 py-4 rounded-full text-sm">Report Found Item</button>
+        </div>
+        <div className="grid grid-cols-4 place-items-center gap-14">
+            {items.map((item) => (
+                <Image src={item.path} height={200} width={200}/>
+            ))}
+        </div>
+        <button className="border-black border px-10 py-3 rounded-full">See All</button>
+      </section>
+      <section className=" flex flex-col items-center justify-center py-10 gap-20">
+        <div className="flex items-center justify-end gap-44 w-4/5 px-6">
+            <h1 className="font-bold text-5xl">Lost Items</h1>
+            <button className="bg-primary text-white px-6 py-4 rounded-full text-sm">Report Lost Item</button>
+        </div>
+        <div className="grid grid-cols-4 place-items-center gap-14">
+            {items.map((item) => (
+                <Image src={item.path} height={200} width={200}/>
+            ))}
+        </div>
+        <button className="border-black border px-10 py-3 rounded-full">See All</button>
+      </section>
+      <section className="flex items-center justify-between px-24 overflow-hidden py-10">
+                <div className="text-black w-[450px] flex flex-col gap-7">
+                    <h1 className="font-medium text-4xl">Follow us</h1>
+                    <div className="flex flex-col ">
+                    <p className="text-slate-600">@https://www.facebook.com/SSGCTUArgao</p>
+                    <p className="text-slate-600">To stay updated with the latest news, promotions, and offerings from the poke , make sure to follow us social media accounts. Don't miss out on any  updates</p>
+                    </div>
+                    <div className="flex pr-48 items-center justify-between">
+                        {
+                            sociallinks.map((links) => (
+                                <Image src={links.image} href={links.href} height={40} width={40}/>
+                            ))
+                        }
+                    </div>
+                </div>
+                <Image src="/imgs/followus_group_pic.png" height={450} width={450}/>
+            </section>
         </div>
     )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,43 @@
+"use client";
 import Image from "next/image";
 import Button from "./_components/Button";
+import { useState } from "react";
 
 export default function Home(){
+
+    const [activeAchievement, setActiveAchievement] = useState(0);
+  const ssgAchievements = [
+    {
+      title: "PROJECT PANULAT 2.0:",
+      description:
+        "The Supreme Student Government orchestrated an inspiring symphony of education with Project Panulat 2.0. The joy radiating from the genuine smiles of children in three schools echoes the success of our mission. In the heart of this activity lies a transformative experience for both givers and receivers. The genuine smiles on every child's face illuminate the profound impact of this initiative. It's not just about handing out pens and papers; it's about instilling a love for learning, igniting curiosity, and creating a positive atmosphere where education becomes an enchanting journey.",
+      image: "/imgs/ssgachievement_1.png",
+    },
+    {
+      title: "PROJECT PANULAT 3.0:",
+      description:
+        "TESTING TESTING TESSTING orchestrated an inspiring symphony of education with Project Panulat 2.0. The joy radiating from the genuine smiles of children in three schools echoes the success of our mission. In the heart of this activity lies a transformative experience for both givers and receivers. The genuine smiles on every child's face illuminate the profound impact of this initiative. It's not just about handing out pens and papers; it's about instilling a love for learning, igniting curiosity, and creating a positive atmosphere where education becomes an enchanting journey.",
+      image: "/imgs/ssgachievement_1.png",
+    },
+    {
+      title: "PROJECT PANULAT 123.0:",
+      description:
+        "BILAT SA BAKAAAAA orchestrated an inspiring symphony of education with Project Panulat 2.0. The joy radiating from the genuine smiles of children in three schools echoes the success of our mission. In the heart of this activity lies a transformative experience for both givers and receivers. The genuine smiles on every child's face illuminate the profound impact of this initiative. It's not just about handing out pens and papers; it's about instilling a love for learning, igniting curiosity, and creating a positive atmosphere where education becomes an enchanting journey.",
+      image: "/imgs/ssgachievement_1.png",
+    },
+  ];
+
+  const handleNextClick = () => {
+    if (activeAchievement < ssgAchievements.length - 1) {
+      setActiveAchievement((prev) => prev + 1);
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (activeAchievement > 0) {
+      setActiveAchievement((prev) => prev - 1);
+    }
+  };
 
     const Merchandise = [
         {picture: "/imgs/tshirt.png", item: "University T-Shirt", price: "250.00"},
@@ -35,7 +71,7 @@ export default function Home(){
                 <p>The SSG Connect is a comprehensive digital platform meticulously designed to streamline student services and enhance engagement within CTU Argao (CTU-AC). Through its innovative features and user-friendly interface, this system aims to empower students by providing convenient access to essential resources and support systems
                 </p>
             </section>
-            <section className=" text-black w-full bg-[#f5f5f5] flex flex-col items-center justify-center py-16 gap-10">
+            <section className=" text-black w-full bg-slate-50 flex flex-col items-center justify-center py-16 gap-10">
                 <h1 className="font-bold text-4xl text-black">University Merchandise</h1>
                 <div className="flex items-center justify-between w-4/5">
                     {Merchandise.map((merchs) =>(
@@ -51,7 +87,70 @@ export default function Home(){
                 </div>
                 <button className="border-black border px-10 py-3 rounded-full">Explore more</button>
             </section>
-            <section className="flex items-center justify-between px-24 overflow-hidden py-10">
+            <section className="relative w-full flex px-16 py-10 gap-10 ">
+        <div className="text-center px-2 flex-1">
+          <h1 className="font-bold text-4xl mb-8">SSG Achievements</h1>
+          <p className="font-bold font-serif">
+            {ssgAchievements[activeAchievement].title}
+          </p>
+          <p className="text-sm mt-2 italic tracking-wide leading-7">
+            {ssgAchievements[activeAchievement].description}
+          </p>
+        </div>
+        <div className="flex-1 flex justify-center">
+          <Image
+            src={ssgAchievements[activeAchievement].image}
+            width={400}
+            height={400}
+          />
+        </div>
+        <div className="flex gap-2 absolute bottom-4 transform left-1/2 -translate-x-1/2">
+          {activeAchievement > 0 && (
+            <button
+              onClick={handlePrevClick}
+              className=" text-white bg-primary px-4 py-3 rounded-full "
+            >
+              ＜
+            </button>
+          )}
+
+          {activeAchievement < ssgAchievements.length - 1 && (
+            <button
+              onClick={handleNextClick}
+              className="text-white bg-primary px-4 py-3 rounded-full "
+            >
+              ＞
+            </button>
+          )}
+        </div>
+      </section>
+      <section className="w-full p-20 bg-white">
+        <div className="flex bg-black p-10 rounded-md overflow-hidden">
+          <div className="flex-1 flex flex-col">
+            <h1 className="font-bold text-4xl text-slate-400 mb-10">
+              Find our locations
+            </h1>
+            <p className="text-slate-500 text-sm mb-8">
+              Lorem ipsum dolor sit amet, consertor adipscing elit, sed do
+              eiusmod tempor incidunt ut labore et dolore magna aliqua
+            </p>
+            <div>
+              <button className="text-white px-8 py-2 bg-primary rounded-3xl text-sm">
+                Locations & Hours
+              </button>
+            </div>
+          </div>
+          <div className="flex-1 flex items-center justify-center relative">
+            <Image
+              src={"/imgs/cta-img.png"}
+              width={500}
+              height={500}
+              className="translate-y-10 absolute -right-7"
+            />
+          </div>
+        </div>
+      </section>
+            <section className="bg-slate-50 flex items-center justify-between px-24 overflow-hidden py-10">
                 <div className="text-black w-[450px] flex flex-col gap-7">
                     <h1 className="font-medium text-4xl">Follow us</h1>
                     <div className="flex flex-col ">
