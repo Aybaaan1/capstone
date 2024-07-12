04c8df481c5fc7ae10485a6ab99db51e736eae3f
Aybaaan1 <knzvanz1@gmail.com>
2024-07-12 18:18:15 +0700

    Resolved merge conflicts

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
-export default function Home(){
-    return (
-        <div>HOME</div>
-    )
+import Comp1 from "./_components/comp1";
+import Input from "./_components/Input";
+function Home(){
+    return <div>
+        <h1>HAHA</h1>
+        {/* <Input file="file" text="text"/> */}
+        <Comp1/>
+    </div>
+
 }
+
+export default Home;
