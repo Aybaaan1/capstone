50500f9b2acd0d1596f5f215b753e8f1af77e805
joshuabriones <joshua.briones@cit.edu>
2024-07-13 03:02:22 +0800

    modal: lost and found modal

---
+++ app/_components/LostFoundForm.jsx
@@ -0,0 +1,174 @@
+import React from "react";
+
+const LostFoundForm = ({ formLabel }) => {
+  return (
+    <form className="w-[80%] bg-red-200 p-10 flex">
+      <div className="p-8 flex-1">
+        <h3 className="text-3xl font-bold mb-4">{formLabel}</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit.
+        </p>
+
+        <div className="mt-6">
+          <label
+            htmlFor="name"
+            className="block text-sm font-semibold text-gray-700"
+          >
+            {" "}
+            Your Name{" "}
+          </label>
+
+          <input
+            type="text"
+            id="name"
+            placeholder="Your name"
+            className="p-2 mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+          />
+        </div>
+        <div className="grid grid-cols-2 mt-4 gap-4">
+          <div>
+            <label
+              htmlFor="department"
+              className="block text-sm font-semibold text-gray-700 "
+            >
+              {" "}
+              Department{" "}
+            </label>
+
+            <select
+              name="department"
+              id="department"
+              className="mt-1.5 w-full p-2 rounded-lg border-gray-300 text-gray-700 sm:text-sm"
+            >
+              <option value="">From what Department?</option>
+              <option value="JM">John Mayer</option>
+              <option value="SRV">Stevie Ray Vaughn</option>
+              <option value="JH">Jimi Hendrix</option>
+              <option value="BBK">B.B King</option>
+              <option value="AK">Albert King</option>
+              <option value="BG">Buddy Guy</option>
+              <option value="EC">Eric Clapton</option>
+            </select>
+          </div>
+          <div>
+            <label
+              htmlFor="place"
+              className="block text-sm font-semibold text-gray-700"
+            >
+              {" "}
+              Place{" "}
+            </label>
+
+            <select
+              name="place"
+              id="place"
+              className="mt-1.5 w-full p-2 rounded-lg border-gray-300 text-gray-700 sm:text-sm"
+            >
+              <option value="">Where you found the item</option>
+              <option value="JM">John Mayer</option>
+              <option value="SRV">Stevie Ray Vaughn</option>
+              <option value="JH">Jimi Hendrix</option>
+              <option value="BBK">B.B King</option>
+              <option value="AK">Albert King</option>
+              <option value="BG">Buddy Guy</option>
+              <option value="EC">Eric Clapton</option>
+            </select>
+          </div>
+          <div>
+            <label
+              htmlFor="date"
+              className="block text-sm font-medium text-gray-700"
+            >
+              {" "}
+              Date{" "}
+            </label>
+
+            <select
+              name="date"
+              id="date"
+              className="mt-1.5 p-2 w-full rounded-lg border-gray-300 text-gray-700 sm:text-sm"
+            >
+              <option value="">Please select</option>
+              <option value="JM">John Mayer</option>
+              <option value="SRV">Stevie Ray Vaughn</option>
+              <option value="JH">Jimi Hendrix</option>
+              <option value="BBK">B.B King</option>
+              <option value="AK">Albert King</option>
+              <option value="BG">Buddy Guy</option>
+              <option value="EC">Eric Clapton</option>
+            </select>
+          </div>
+          <div>
+            <label
+              htmlFor="time"
+              className="block text-sm font-semibold text-gray-700"
+            >
+              {" "}
+              Time{" "}
+            </label>
+
+            <select
+              name="time"
+              id="time"
+              className="mt-1.5 p-2 w-full rounded-lg border-gray-300 text-gray-700 sm:text-sm"
+            >
+              <option value="">Please select</option>
+              <option value="JM">John Mayer</option>
+              <option value="SRV">Stevie Ray Vaughn</option>
+              <option value="JH">Jimi Hendrix</option>
+              <option value="BBK">B.B King</option>
+              <option value="AK">Albert King</option>
+              <option value="BG">Buddy Guy</option>
+              <option value="EC">Eric Clapton</option>
+            </select>
+          </div>
+          <button className="bg-primary text-white py-2 col-span-2 rounded-3xl mt-4">
+            Report Now
+          </button>
+        </div>
+      </div>
+      <div className="flex-1 h-full">
+        <h3 className="text-xl font-bold mb-4">Upload files</h3>
+        <div className="relative my-6 w-full h-full">
+          <input
+            id="id-dropzone01"
+            name="file-upload"
+            type="file"
+            className="hidden"
+          />
+          <label
+            htmlFor="id-dropzone01"
+            className="relative flex cursor-pointer flex-col items-center gap-4 rounded border border-dashed border-slate-300 px-3 py-36 text-center text-sm font-medium transition-colors"
+          >
+            <span className="inline-flex h-12 items-center justify-center self-center rounded-full bg-slate-100/70 px-3 text-slate-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                aria-label="File input icon"
+                role="graphics-symbol"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
+                />
+              </svg>
+            </span>
+            <span className="text-slate-500">
+              Drag & drop or
+              <span className="text-primary"> upload a file</span>
+            </span>
+          </label>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default LostFoundForm;
--- app/aboutus/page.jsx
+++ app/aboutus/page.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import LostFoundForm from "../_components/LostFoundForm";
 
 const AboutUs = () => {
   const [activeAchievement, setActiveAchievement] = useState(0);
@@ -24,6 +25,13 @@ const AboutUs = () => {
         "BILAT SA BAKAAAAA orchestrated an inspiring symphony of education with Project Panulat 2.0. The joy radiating from the genuine smiles of children in three schools echoes the success of our mission. In the heart of this activity lies a transformative experience for both givers and receivers. The genuine smiles on every child's face illuminate the profound impact of this initiative. It's not just about handing out pens and papers; it's about instilling a love for learning, igniting curiosity, and creating a positive atmosphere where education becomes an enchanting journey.",
       image: "/imgs/ssgachievement_1.png",
     },
+  ];
+
+  const sociallinks = [
+    { image: "/imgs/fb-icon.png", href: "/" },
+    { image: "/imgs/twitter-icon.png", href: "/" },
+    { image: "/imgs/instagram-icon.png", href: "/" },
+    { image: "/imgs/youtube-icon.png", href: "/" },
   ];
 
   const handleNextClick = () => {
@@ -128,6 +136,32 @@ const AboutUs = () => {
             />
           </div>
         </div>
+      </section>
+      <section className="flex items-center justify-between px-24 overflow-hidden py-10">
+        <div className="text-black w-[450px] flex flex-col gap-7">
+          <h1 className="font-medium text-4xl">Follow us</h1>
+          <div className="flex flex-col ">
+            <p className="text-slate-600">
+              @https://www.facebook.com/SSGCTUArgao
+            </p>
+            <p className="text-slate-600">
+              To stay updated with the latest news, promotions, and offerings
+              from the poke , make sure to follow us social media accounts.
+              Don't miss out on any updates
+            </p>
+          </div>
+          <div className="flex pr-48 items-center justify-between">
+            {sociallinks.map((links) => (
+              <Image
+                src={links.image}
+                href={links.href}
+                height={40}
+                width={40}
+              />
+            ))}
+          </div>
+        </div>
+        <Image src="/imgs/followus_group_pic.png" height={450} width={450} />
       </section>
     </div>
   );
