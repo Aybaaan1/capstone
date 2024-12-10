a4f0456613e99f9f6f83621380e8e81b87314f85
Aybaaan1 <knzvanz1@gmail.com>
2024-12-10 19:35:19 +0700

    signin

--- app/(backend)/api/signin/route.js
+++ app/(backend)/api/signin/route.js
@@ -2,7 +2,14 @@ import db from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  const signin = await db.signin.findMany();
-
-  return NextResponse.json({ signin });
+  try {
+    const users = await db.user.findMany(); // Fetch all users
+    return NextResponse.json({ users });
+  } catch (error) {
+    console.error("Error fetching user records:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch user records" },
+      { status: 500 }
+    );
+  }
 }
